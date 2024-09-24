6dadc73227c4c665adfe33039587191adc012974
PersuitTheWind <221502014@smail.nju.edu.cn>
2024-09-25 00:25:24 +0800

    daily change

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,60 +6,59 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return writer_close;
 }
 
 void Writer::push( string data )
 {
+  if (is_closed()) return;
+  uint64_t s = available_capacity();
+  uint64_t x = min (s , data.size());
+  buffer_.append(data,substr(0,x));
+  push_num += x;
   // Your code here.
-  (void)data;
+  //(void)data;
   return;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  writer_close = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return push_num;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return writer_close && buffer_.empty();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return pop_num;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  return string_view(buffer_);
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  uint_64 x = min(len, buffer_.size());
+  buffer.erase(0,x);
+  pop_num += x;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+   return buffer_.size();
 }
