935110f7b81700d93a8361ca3ff44b07b7a9c087
PersuitTheWind <221502014@smail.nju.edu.cn>
2024-10-09 20:55:51 +0800

    check1 - passed

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -9,27 +9,27 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   if (is_last_substring) ed_index = ed;
   if (first_unassembled_index() == ed_index ) { output_.writer().close(); return; }
   std::string data1 = data;
-  if (last_index <= first_unassembled_index() || first_index >= first_unacceptable_index() || data.empty() ) {
-     end_of_a_bytestream ();
+  if (last_index < first_unassembled_index() || first_index >= first_unacceptable_index() || data.empty() ) {
+    // end_of_a_bytestream ();
      return;
   }
   if ( st <= first_unassembled_index()){
      data1 = data1.substr(first_unassembled_index() - first_index);
      st = first_unassembled_index();
   }
-  if ( ed >= first_unacceptable_index()){
-     data1 = data1.substr( 0 ,data1.length() - ( last_index - first_unacceptable_index() ) );
-     ed = first_unacceptable_index();
+  if ( ed > first_unacceptable_index()){
+     data1 = data1.substr( 0 ,first_unacceptable_index() - st );
+     ed = st + data1.length();
   }
   
   if (st > first_unassembled_index()){
      if (! buffer_.empty()){
         auto it = buffer_.upper_bound (st);
-        it--;
+        if (it != buffer_.begin()) it--;
         uint64_t st0 = it -> first;
         uint64_t ed0 = st0 + it -> second.length();
-        if(ed0 >= st + 1){
-           if(ed0 < ed) data1 = data1.substr(ed0 - st + 1);
+        if(st0 <= st && ed0 >= st){
+           if(ed0 < ed) data1 = data1.substr(ed0 - st);
            else {
               end_of_a_bytestream ( );
               return;
@@ -38,18 +38,18 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
            st = it -> first;
            it = buffer_ . erase(it);
         }
-        else it++;
+        it = buffer_.upper_bound (st);
         while(it != buffer_.end()){
             if ( ed < it -> first ) break;
-            if ( ed < it -> first + it -> second.length()){
+            if ( ed <= it -> first + it -> second.length()){
                 data1 += it->second.substr(ed - it -> first);
-                ed = it -> first + it -> second.length(); 
+                ed = st + data1.length(); 
             }
             it = buffer_ . erase(it);
         }
      }
      buffer_.insert({ st , data1});    
-     end_of_a_bytestream ();
+     //end_of_a_bytestream ();
      return;
   }
   else output_.writer().push( data1 );
@@ -73,13 +73,21 @@ uint64_t Reassembler::first_unacceptable_index(){
 void Reassembler::check_buffer(){
     auto it  = buffer_.begin();
     while( it != buffer_.end() && it -> first <= first_unassembled_index()){
-          if ( it -> first + it -> second.length() > first_unassembled_index()){
+          if ( it -> first + it -> second.length() >= first_unassembled_index()){
                std::string ss = it -> second ;
                ss = ss.substr(first_unassembled_index() - it -> first);
                output_.writer().push(ss);
           }
+      
           it = buffer_.erase(it);
-    }         
+    }       
+    it = buffer_.begin();
+    if (!buffer_.empty() && it -> first <= first_unassembled_index() ){
+       std::string ss = it -> second ;
+       ss = ss.substr(first_unassembled_index() - it -> first);
+       output_.writer().push(ss);
+       buffer_.erase ( buffer_.begin());
+    }
 }
 void Reassembler::end_of_a_bytestream(){
   if (buffer_.empty() && first_unassembled_index() == ed_index ) output_.writer().close();
