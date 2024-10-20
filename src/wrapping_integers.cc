4f51ac8bf1d2bac8a7909b7173a6937d2b757047
PersuitTheWind <221502014@smail.nju.edu.cn>
2024-10-20 20:54:00 +0800

    check2 wrap & unwrap pass

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -2,18 +2,20 @@
 
 using namespace std;
 
+const uint64_t WRAP_LENGTH = 1ul << 32; 
+
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  uint32_t nn = n;
+  return zero_point + nn;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t check_seq = wrap(checkpoint,zero_point).raw_value_;
+  int32_t distance = raw_value_ - check_seq; 
+  int64_t abs_seqno = checkpoint + distance ;
+  if  ( abs_seqno < 0 )
+      abs_seqno += WRAP_LENGTH;
+  return abs_seqno;
 }
